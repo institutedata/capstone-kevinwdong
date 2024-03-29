09d1e0ebad9fae283d3e90316cb4bc8eaa4d619e
mrheinekev@gmail.com <mrheinekev@gmail.com>
2024-03-29 19:10:27 +1300

    Set up login authentication and create new user and jwt token

--- Server/routes/userRoute.js
+++ Server/routes/userRoute.js
@@ -3,21 +3,33 @@ let router = Router();
 import { userController } from "../controllers/index.js";
 
 
-router.get('/get', (req, res) => {
-   userController.getUser(req, res);
+router.post('/register', (req, res) => {
+   userController.registerUser(req, res);
 })
 
-router.post('/create', (req, res) => {
-   userController.createUser(req, res);
+router.post('/login', (req, res) => {
+   userController.authenticateUser(req, res);
 })
 
-router.put('/update', (req, res) => {
-   userController.updateUser(req, res)
+router.get('/profile', (req, res) => {
+   userController.updateUserProfile(req, res);
 })
 
-router.delete('/delete', (req, res) => {
-   userController.deleteUser(req, res)
-})
+// router.get('/get', (req, res) => {
+//    userController.getUser(req, res);
+// })
+
+// router.post('/create', (req, res) => {
+//    userController.createUser(req, res);
+// })
+
+// router.put('/update', (req, res) => {
+//    userController.updateUser(req, res)
+// })
+
+// router.delete('/delete', (req, res) => {
+//    userController.deleteUser(req, res)
+// })
 
 
 
--- Server/server.js
+++ Server/server.js
@@ -6,6 +6,7 @@ import eventRoutes from "./routes/eventRoute.js";
 import cors from "cors";
 import { dbConnect } from "./dbConnect.js";
 import { config as dotenvConfig } from "dotenv";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 dotenvConfig();
 
 const app = express();
@@ -14,13 +15,16 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use("/api/user", userRoutes);
+app.use("/api/users", userRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/event", eventRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to myMongoDB application.".green });
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
