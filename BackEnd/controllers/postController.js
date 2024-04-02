c746a5f6bac5485e5d4e24db8a9125fa8f9b4d8a
mrheinekev@gmail.com <mrheinekev@gmail.com>
2024-04-02 19:57:17 +1300

    Set up create post get all post and a user's post with controllers and routes. without verifytoken

--- BackEnd/controllers/postController.js
+++ BackEnd/controllers/postController.js
@@ -1,2 +1,58 @@
+import Post from "../models/Post.js";
+import User from "../models/user.js";
+import { errorHandler } from "../utils/error.js";
+
+//@desc     Create a post
+//@route    POST /posts/create
+export const createPost = async (req, res) => {
+  try {
+    const { userId, description, postImage } = req.body;
+    const user = await User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      description,
+      userImage: user.userImage,
+      postImage,
+      likes: {},
+      comments: [],
+    });
+    await newPost.save();
+
+    const post = await Post.find();
+    res.status(201).json(post);
+  } catch (error) {
+    next(errorHandler(400, error.message));
+  }
+};
+
+
+
+//@desc     Get all posts
+//route    GET /posts
+export const getFeedPosts = async (req, res) => {
+  try {
+    const post = await Post.find();
+    res.status(200).json(post);
+  } catch (error) {
+    next(errorHandler(400, error.message));
+  }
+};
+
+
+//@desc     Get a user's posts
+//@route    GET /posts/:userId/posts
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const post = await Post.find({ userId });
+    res.status(200).json(post);
+  } catch (err) {
+    next(errorHandler(400, error.message));
+  }
+};
+
 
 
--- BackEnd/routes/postRoutes.js
+++ BackEnd/routes/postRoutes.js
@@ -1,8 +1,18 @@
 import express from "express";
+import { createPost, getFeedPosts, getUserPosts } from "../controllers/postController.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 
 const router = express.Router();
 
+//@desc    Create a post
+router.post('/create', createPost);
+
+//@desc    Get all posts
+router.get("/", getFeedPosts);
+
+//@desc    Get a user's posts
+router.get("/:userId/posts", getUserPosts);
 
 
 export default router;
